807d91df800928424f8d89df563419dd2b4b0e8d
jspiew <jacek.spiewak@outlook.com>
2019-02-10 09:52:48 +0100

    adds padding to app body

--- src/App.tsx
+++ src/App.tsx
@@ -77,6 +77,7 @@ class App extends React.Component<{},{jpk:IJPK}> {
           <h1 className="App-title">Troche prostsze JPK</h1>
           <link rel="stylesheet" href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-core/9.6.1/css/fabric.min.css"/>
         </header>
+        <div className="App-main">
           <JpkNaglowek jpk={this.state.jpk} updateJpk={this._updateJPK}/>
           <FakturyZakupu 
             jpk = {this.state.jpk}
@@ -95,13 +96,14 @@ class App extends React.Component<{},{jpk:IJPK}> {
             updateSellInvoice={this._updateSellInvoice}
             updateJpk={this._updateJPK}
           />
-        <DefaultButton onClick={this._getCSV} className="generateCSVButton" text="Wygeneruj CSV" iconProps={{ iconName: 'DownloadDocument', iconType: IconType.default }}/>
-        {window.location.search.indexOf("dbg") >= 0 && <code>
-          <pre>
-            {JSON.stringify(this.state.jpk,null,4)}
-          </pre>
-        </code>
-        }
+          <DefaultButton onClick={this._getCSV} className="generateCSVButton" text="Wygeneruj CSV" iconProps={{ iconName: 'DownloadDocument', iconType: IconType.default }}/>
+          {window.location.search.indexOf("dbg") >= 0 && <code>
+            <pre>
+              {JSON.stringify(this.state.jpk,null,4)}
+            </pre>
+          </code>
+          }
+        </div>
       </div>
     );
   }
